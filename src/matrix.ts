7b4e232761a1f0ce1f27c7add5c409968bb7f175
jvega <jose.vega@here.com>
2022-08-27 11:29:40 +0200

    Render a square + module addition
    Render a white square on a black background.
    
    Added the following modules responsible for different steps in the setup
    and execution of the rendering pipeline:
    - Matrix utilities
    - Shader functions
    - Scene functions

---
+++ src/matrix.ts
@@ -0,0 +1,45 @@
+import { mat4 } from "gl-matrix";
+
+/**
+ * Create a perspective matrix, a special matrix that is
+ * used to simulate the distortion of perspective in a camera.
+ */
+export function createProjectionMatrix(gl: WebGLRenderingContext): mat4 {
+    // Our field of view is 45 degrees, with a width/height
+    // ratio that matches the display size of the canvas
+    // and we only want to see objects between 0.1 units
+    // and 100 units away from the camera.
+
+    const fieldOfView = 45 * Math.PI / 180;   // in radians
+    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    const zNear = 0.1;
+    const zFar = 100.0;
+    const projectionMatrix = mat4.create();
+
+    mat4.perspective(projectionMatrix,
+                    fieldOfView,
+                    aspect,
+                    zNear,
+                    zFar);
+
+    return projectionMatrix;
+}
+
+export function createModelViewMatrix(): mat4 {
+    // Set the drawing position to the "identity" point, which is
+    // the center of the scene.
+    const modelViewMatrix = mat4.create();
+
+    // Now move the drawing position a bit to where we want to
+    // start drawing the square.
+
+    mat4.translate(modelViewMatrix,     // destination matrix
+                    modelViewMatrix,     // matrix to translate
+                    [-0.0, 0.0, -8.0]);  // amount to translate
+
+    // mat4.rotateY(modelViewMatrix,
+    //                 modelViewMatrix,
+    //                 -45 * Math.PI / 180);
+
+    return modelViewMatrix;
+}
---
+++ src/scene.ts
@@ -0,0 +1,56 @@
+import { createModelViewMatrix, createProjectionMatrix } from "./matrix";
+import { ShaderProgramInfo } from "./shader";
+
+function setupScene(gl: WebGLRenderingContext) {
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Set clear color to black, fully opaque
+    gl.clearDepth(1.0);                 // Clear everything
+    gl.enable(gl.DEPTH_TEST);           // Enable depth testing
+    gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
+    // Clear the canvas before we start drawing on it.
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+}
+
+export function drawScene(gl: WebGLRenderingContext, programInfo: ShaderProgramInfo, buffer: WebGLBuffer) {
+    setupScene(gl);
+
+    const projectionMatrix = createProjectionMatrix(gl);
+    const modelViewMatrix = createModelViewMatrix();
+
+    // Tell WebGL how to pull out the positions from the position
+    // buffer into the vertexPosition attribute.
+
+    const numComponents = 3;  // pull out 2 values per iteration
+    const type = gl.FLOAT;    // the data in the buffer is 32bit floats
+    const normalize = false;  // don't normalize
+    const stride = 0;         // how many bytes to get from one set of values to the next
+                                    // 0 = use type and numComponents above
+    const offset = 0;         // how many bytes inside the buffer to start from
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.vertexAttribPointer(
+        programInfo.attribLocations.vertexPosition,
+        numComponents,
+        type,
+        normalize,
+        stride,
+        offset);
+    gl.enableVertexAttribArray(
+        programInfo.attribLocations.vertexPosition);
+
+    // Tell WebGL to use our program when drawing
+    gl.useProgram(programInfo.program);
+
+    // Set the shader uniforms
+    gl.uniformMatrix4fv(
+        programInfo.uniformLocations.projectionMatrix,
+        false,
+        projectionMatrix);
+    gl.uniformMatrix4fv(
+        programInfo.uniformLocations.modelViewMatrix,
+        false,
+        modelViewMatrix);
+
+
+    const arrayOffset = 0;
+    const vertexCount = 4;
+    gl.drawArrays(gl.TRIANGLE_STRIP, arrayOffset, vertexCount);
+}
