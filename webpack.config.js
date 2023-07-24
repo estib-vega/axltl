9ff0d837b52bf0b492b837e0ba837e77cfc6b277
Esteban Vega <estib@air.fritz.box>
2023-07-24 20:16:06 +0200

    Re-add missing deps and some fixes
    - Fix the webpack config
    - Set a fixed host and port for the web server
    - Use CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,110 +1,101 @@
 const path = require("path");
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
-
+const { Configuration } = require("webpack");
+/**
+ * @type {Configuration}
+ */
 const baseConfig = {
-    target: "web",
-    entry: [
-        path.resolve(__dirname, "./src/html/index.html"),
-        path.resolve(__dirname, "./src/index.ts"),
-        path.resolve(__dirname, "./src/css/index.less"),
-    ],
-    output: {
-        path: path.resolve(__dirname, "build"),
-        filename: "index.js",
-    },
-    resolve: {
-        plugins: [
-            new TsconfigPathsPlugin({
-                extensions: [".ts", ".tsx", ".js"],
-            }),
-        ],
+  target: "web",
+  entry: [
+    path.resolve(__dirname, "./src/html/index.html"),
+    path.resolve(__dirname, "./src/index.ts"),
+    path.resolve(__dirname, "./src/css/index.css"),
+  ],
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "index.js",
+  },
+  resolve: {
+    plugins: [
+      new TsconfigPathsPlugin({
         extensions: [".ts", ".tsx", ".js"],
-        fallback: { path: require.resolve("path-browserify") },
-    },
-    externals: {
-        fs: "undefined",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    {
-                        loader: "style-loader",
-                    },
-                    {
-                        loader: "css-loader",
-                    },
-                    {
-                        loader: "less-loader",
-                        options: {
-                            strictMath: true,
-                            noIeCompat: true,
-                        },
-                    },
-                ],
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: "style-loader",
-                    },
-                    {
-                        loader: "css-loader",
-                    },
-                ],
-            },
-            {
-                test: /\.(ttf|eot|woff|woff2|otf)(\?v=[^\.]+\.[^\.]+\.[^\.]+)?$/,
-                use: {
-                    loader: "file-loader",
-                    options: {
-                        name: "[name].[hash:8].[ext]",
-                    },
-                },
-            },
-            {
-                test: /\.(jpe?g|svg|png)$/,
-                use: {
-                    loader: "file-loader",
-                    options: {
-                        name: "[name].[hash].[ext]",
-                    },
-                },
-            },
-            {
-                test: /\.html$/,
-                use: {
-                    loader: "file-loader",
-                    options: {
-                        name: "[name].[ext]",
-                    },
-                },
-            },
+      }),
+    ],
+    extensions: [".ts", ".tsx", ".js"],
+    fallback: { path: require.resolve("path-browserify") },
+  },
+  externals: {
+    fs: "undefined",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
         ],
-    },
-    devServer: {
-        allowedHosts: "all",
-        port: 5000,
-        static: [
-            {
-                directory: path.join(__dirname, "./build"),
-                watch: true,
-            },
-            {
-                directory: path.join(__dirname, "./src"),
-                watch: true,
-            },
-        ],
-    },
+      },
+      {
+        test: /\.(ttf|eot|woff|woff2|otf)(\?v=[^\.]+\.[^\.]+\.[^\.]+)?$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash:8].[ext]",
+          },
+        },
+      },
+      {
+        test: /\.(jpe?g|svg|png)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+          },
+        },
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    allowedHosts: "all",
+    host: "127.0.0.1",
+    port: 5000,
+    static: [
+      {
+        directory: path.join(__dirname, "./build"),
+        watch: true,
+      },
+      {
+        directory: path.join(__dirname, "./src"),
+        watch: true,
+      },
+    ],
+  },
 };
 
 module.exports = [
-    { ...baseConfig, name: "development", mode: "none", devtool: "source-map" },
-    { ...baseConfig, name: "production", mode: "production", optimization: { minimize: true } },
+  { ...baseConfig, name: "development", mode: "none", devtool: "source-map" },
+  {
+    ...baseConfig,
+    name: "production",
+    mode: "production",
+    optimization: { minimize: true },
+  },
 ];
