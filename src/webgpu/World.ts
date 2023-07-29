55c9db3250e567c914c0d9e0148dfbfe2777b8d8
estib <stron@me.com>
2023-07-29 10:04:49 +0200

    Add multiple cubes and a way to update them
    Create 64 cubes and methods to update them before every frame

--- src/webgpu/World.ts
+++ src/webgpu/World.ts
@@ -1,9 +1,16 @@
-import { Vec3, WebGPUMat, WebGLMat, toRadians } from "src/util";
+import { Vec3, WebGPUMat, toRadians } from "src/util";
 import Engine from "./Engine";
 import { BoundUniformBuffer } from "./buffer";
-import * as Body from "src/body";
+import BaseBody from "src/body/BaseBody";
+import { Renderable } from "./renderPass";
 
 const TRANSFORMATION_MATRIX_SIZE = 4 * 4;
+
+interface BeforeUpdateCallbackParams {
+  readonly bodies: BaseBody[];
+}
+
+type BeforeUpdateCallback = (params: BeforeUpdateCallbackParams) => void;
 
 export default class World {
   private static singleInstance: World | undefined = undefined;
@@ -14,18 +21,22 @@ export default class World {
   private engine: Engine;
   private transformMatrixUniform: BoundUniformBuffer;
 
+  private bodies: Set<BaseBody>;
+
   private constructor(engine: Engine) {
     this.engine = engine;
 
     this.rotationAngle = 0;
     this.rotationAxis = { x: 0, y: -1.0, z: 0.0 };
-    this.translationVector = { x: 0, y: 0, z: -17 };
+    this.translationVector = { x: 0, y: 0, z: -20 };
 
     this.transformMatrixUniform = engine.createUniform(
       TRANSFORMATION_MATRIX_SIZE,
       0,
       0
     );
+
+    this.bodies = new Set();
   }
 
   /**
@@ -70,30 +81,59 @@ export default class World {
   }
 
   /**
+   * Add renderable bodies to the world.
+   */
+  public addBodies(...bodies: BaseBody[]) {
+    for (const body of bodies) {
+      this.bodies.add(body);
+    }
+  }
+
+  /**
+   * Get all renderables from the added bodies
+   */
+  private getRenderables(): Renderable[] {
+    const result: Renderable[] = [];
+    for (const body of this.bodies) {
+      result.push(body.getRenderable(this.engine));
+    }
+    return result;
+  }
+
+  /**
+   * Update the world and all the bodies inside.
+   */
+  private update() {
+    this.updateTransformMatrixUniform();
+    for (const body of this.bodies) {
+      body.update();
+    }
+  }
+
+  /**
+   * Add to the rotation angle
+   */
+  rotate(angle: number, axis: Vec3) {
+    this.rotationAngle += angle;
+    if (this.rotationAngle > 360) {
+      this.rotationAngle -= 360;
+    }
+    if (this.rotationAngle < 0) {
+      this.rotationAngle = 360 - this.rotationAngle;
+    }
+    this.rotationAxis = axis;
+  }
+
+  /**
    * Start render loop.
    */
-  public show() {
-    const cube1 = new Body.ColorCube();
-    cube1.position({ x: -2, y: 0, z: 0 });
-    const cube2 = new Body.ColorCube();
-    cube2.position({ x: 2, y: 0, z: 0 });
-    const renderable1 = cube1.getRenderable(this.engine);
-    const renderabl2 = cube2.getRenderable(this.engine);
-
-    const cubes = [cube1, cube2];
-    const renderables = [renderable1, renderabl2];
-
-    const rotationVelocity = 1;
+  public show(beforeUpdate: BeforeUpdateCallback) {
+    const renderables = this.getRenderables();
 
     const frame = () => {
-      this.updateTransformMatrixUniform();
-      cube1.rotate(rotationVelocity, { x: 1, y: 0, z: 0 });
-      cube2.rotate(rotationVelocity, { x: 0, y: 0, z: 1 });
-      for (const cube of cubes) {
-        cube.update();
-      }
+      beforeUpdate({ bodies: Array.from(this.bodies) });
+      this.update();
       this.engine.doRenderPass(this.transformMatrixUniform, renderables);
-      this.rotationAngle += 0.5;
       requestAnimationFrame(frame);
     };
     requestAnimationFrame(frame);
--- src/webgpu/index.ts
+++ src/webgpu/index.ts
@@ -1,6 +1,25 @@
 import World from "./World";
+import * as Body from "src/body";
 
 export async function webgpuMain() {
   const world = await World.instance();
-  world.show();
+  let yOffset = 0;
+  let zOffset = 0;
+  const numOfCubes = 4 * 4 * 4;
+  for (let i = 0; i < numOfCubes; i++) {
+    if (i % 4 === 0) yOffset = (yOffset + 1) % 4;
+    if (i % 16 === 0) zOffset++;
+    const cube = new Body.ColorCube();
+    const x = (i % 4) * 4 - 6;
+    const z = zOffset * 4 - 10;
+    const y = yOffset * 4 - 6;
+    cube.position({ x, y, z });
+    world.addBodies(cube);
+  }
+  world.show(({ bodies }) => {
+    for (const body of bodies) {
+      body.rotate(4, { x: 1, y: 0.5, z: -1 });
+    }
+    world.rotate(0.5, { x: 0.3, y: 1, z: 0.5});
+  });
 }
